c1f4675431fc7b4bffb245a40793463eb293cf80
hannahazan <hannahazan2@gmail.com>
2025-01-15 15:31:49 +0100

    quest 3

---
+++ src/main/java/org/wildcodeschool/MyBlogApplication/repository/ArticleRepository.java
@@ -0,0 +1,7 @@
+package org.wildcodeschool.MyBlogApplication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.wildcodeschool.MyBlogApplication.model.Article;
+
+public interface ArticleRepository extends JpaRepository<Article,Long> {
+}
