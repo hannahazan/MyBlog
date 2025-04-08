6cbea8c30a2e54a195452428bc54a237766c4088
hannahazan <hannahazan2@gmail.com>
2025-04-08 15:53:50 +0200

    Quest 13 the real one

--- src/main/java/org/wildcodeschool/MyBlogApplication/security/CustomUserDetailsService.java
+++ src/main/java/org/wildcodeschool/MyBlogApplication/security/CustomUserDetailsService.java
@@ -3,8 +3,10 @@ package org.wildcodeschool.MyBlogApplication.security;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import org.wildcodeschool.MyBlogApplication.repository.UserRepository;
 
+@Service
 public class CustomUserDetailsService  implements UserDetailsService {
 
     private final UserRepository userRepository;
--- src/main/java/org/wildcodeschool/MyBlogApplication/security/JwtAuthenticationFilter.java
+++ src/main/java/org/wildcodeschool/MyBlogApplication/security/JwtAuthenticationFilter.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,15 +16,15 @@ import java.util.Date;
 @Component
 public class JwtAuthenticationFilter  extends OncePerRequestFilter {
     private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
+    private final CustomUserDetailsService customUserDetailsService;
 
-    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+    public JwtAuthenticationFilter(CustomUserDetailsService customUserDetailsService,JwtService jwtService) {
         this.jwtService = jwtService;
-        this.userDetailsService = userDetailsService;
+        this.customUserDetailsService =customUserDetailsService;
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authHeader= request.getHeader("Authorization");
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
@@ -33,7 +32,7 @@ public class JwtAuthenticationFilter  extends OncePerRequestFilter {
             String username = jwtService.extractClaims(jwt).getSubject();
 
             if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
                 if (jwtService.extractClaims(jwt).getExpiration().after(new Date())) {
                     UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                             userDetails, null, userDetails.getAuthorities()
